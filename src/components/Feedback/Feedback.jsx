6f1ce7e8c005bde4af178aa54971422e223c68ac
Urban <urban.mutagen@gmail.com>
2025-04-03 22:05:39 +0300

    2

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,28 @@
+// import PropTypes from "prop-types";
+import css from "./Feedback.module.css";
+
+const Feedback = ({ feedbackObj, feedbackTotal, feedbackPositive }) => {
+  const { good, neutral, bad } = feedbackObj;
+
+  return (
+    <>
+      <div className={css.divContainer}>
+        <p className={css.feedbackParagraf}>❤️ Good: {good}</p>
+        <p className={css.feedbackParagraf}>😒 Neutral: {neutral}</p>
+        <p className={css.feedbackParagraf}>😢 Bad: {bad}</p>
+      </div>
+      <div className={css.divContainer}>
+        <p className={css.feedbackParagraf}>🤩 Total: {feedbackTotal}</p>
+        <p className={css.feedbackParagraf}>💕 Positive: {feedbackPositive}%</p>
+      </div>
+    </>
+  );
+};
+
+export default Feedback;
+
+// Feedback.protoType = {
+//   feedbackObj: PropTypes.objectOf(PropTypes.number),
+//   feedbackTotal: PropTypes.number,
+//   feedbackPositive: PropTypes.number,
+// };
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,7 @@
+import css from "./Notification.module.css";
+
+const Notification = () => {
+  return <p className={css.paragraf}>No feedback yet 😿</p>;
+};
+
+export default Notification;
